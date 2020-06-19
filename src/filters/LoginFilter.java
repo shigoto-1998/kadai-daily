6ab596df80053cc7f7afd6b74b4302986ec2627d
yuya.kuwamoto <baseball.love.666@icloud.com>
2020-06-19 15:16:37 +0900

    Add login / logout link

--- src/filters/LoginFilter.java
+++ src/filters/LoginFilter.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import models.Employee;
+
 /**
  * Servlet Filter implementation class LoginFilter
  */
@@ -41,28 +42,28 @@ public class LoginFilter implements Filter {
         String context_path = ((HttpServletRequest)request).getContextPath();
         String servlet_path = ((HttpServletRequest)request).getServletPath();
 
-        if(!servlet_path.matches("/css.*")){   //cssフォルダ内は認証処理から除外する
+        if(!servlet_path.matches("/css.*")) {       // CSSフォルダ内は認証処理から除外する
             HttpSession session = ((HttpServletRequest)request).getSession();
 
-            //セッションスコープに保存された従業員（ログインユーザー）情報を取得
+            // セッションスコープに保存された従業員（ログインユーザ）情報を取得
             Employee e = (Employee)session.getAttribute("login_employee");
 
-            if(!servlet_path.equals("/login")) {    //ログイン画面以外について
-                //ログアウトしている状態であれば
-                //ログイン画面にリダイレクト
-                if(e == null){
+            if(!servlet_path.equals("/login")) {        // ログイン画面以外について
+                // ログアウトしている状態であれば
+                // ログイン画面にリダイレクト
+                if(e == null) {
                     ((HttpServletResponse)response).sendRedirect(context_path + "/login");
                     return;
                 }
 
-                //従業員管理の機能は管理者のみが閲覧できるようにする
+                // 従業員管理の機能は管理者のみが閲覧できるようにする
                 if(servlet_path.matches("/employees.*") && e.getAdmin_flag() == 0) {
                     ((HttpServletResponse)response).sendRedirect(context_path + "/");
                     return;
                 }
-            }else {          //ログイン画面について
-                //ログインしているのにログイン画面を表示させようとした場合は
-                //システムのトップページにリダイレクト
+            } else {                                    // ログイン画面について
+                // ログインしているのにログイン画面を表示させようとした場合は
+                // システムのトップページにリダイレクト
                 if(e != null) {
                     ((HttpServletResponse)response).sendRedirect(context_path + "/");
                     return;
